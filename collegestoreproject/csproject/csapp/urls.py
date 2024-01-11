b7055a74e4f634248aad49dedb34a8a124bbc330
Aakash <mr.aakash726@gmail.com>
2024-01-11 11:36:29 +0530

    v1

--- collegestoreproject/csproject/csapp/urls.py
+++ collegestoreproject/csproject/csapp/urls.py
@@ -3,4 +3,11 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
+    path('form/',views.form,name='form'),
+    path('message/',views.message,name='message'),
+    path('logout/',views.logout,name='logout')
+
+
 ]
--- collegestoreproject/csproject/csapp/views.py
+++ collegestoreproject/csproject/csapp/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import  auth
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def form(request):
+    return render(request,'form.html')
+def message(request):
+    return render(request,'message.html')
+def login(request):
+    if request.method=="POST":
+        usernamel=request.POST['username_l']
+        passwordl=request.POST['password_l']
+        userdb=auth.authenticate(username=usernamel,password=passwordl)
+        if userdb is not None:
+            auth.login(request,userdb)
+            return redirect('form')
+        else:
+            return redirect('login')
+    return render(request,'login.html')
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username_r']
+        password=request.POST['password_r']
+        cpassword=request.POST['cpassword_r']
+        if password==cpassword:
+            user=User.objects.create_user(username=username,password=password)
+            user.save()
+            return redirect('login')
+        else:
+            return redirect('/')
+    return render(request,'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
